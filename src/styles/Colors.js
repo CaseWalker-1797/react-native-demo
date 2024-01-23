d0b5040bb1803607a49644aa9e1c5d28d86c75ab
CaseWalker-1797 <veerkamdar123@gmail.com>
2024-01-23 15:20:20 +0530

    ```

--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -5,4 +5,4 @@ export const accentColor = '#51C833';
 export const bgColor = '#0D2A3F';
 export const textColor = '#FFFFFF';
 export const inactiveTabColor='#CBCFE1CC';
-export const checkboxFillColor='rgba(203, 207, 225, 0.60)'
+export const checkboxFillColor='rgba(203, 207, 225, 0.60)';
